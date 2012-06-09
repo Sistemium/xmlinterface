63e5c97b7fe13bd9b379a530b253b16184a77a79
DeadCat93 <alexey.i.panshin@gmail.com>
2012-06-09 10:48:07 +0400

    Переписал csvMultibankTerminalForAmendmentClaim

--- sql/csvMultibankTerminalForAmendmentClaim.sql
+++ sql/csvMultibankTerminalForAmendmentClaim.sql
@@ -1,16 +1,22 @@
-alter FUNCTION [dbo].[csvMultibankTerminalForAmendmentClaim] 
+alter function dbo.csvMultibankTerminalForAmendmentClaim
 (	
 	@date datetime,
-	@summ datetime
+	@summ decimal(18,2)
 )
-RETURNS TABLE 
-AS
-RETURN 
+returns table 
+as
+return 
 (
 select top(1) dt.terminalid
-from dbo.distribday_trace dt join dbo.bankaccountterminal t on dt.terminalid = t.terminalid
-where t.mainSubdealer = 7
-  and dt.ddate = CONVERT(VARCHAR(10), @date, 121)
-  and t.src_system in (1,2)
-  and dt.today_summ > @summ
-order by dt.today_summ desc
+  from dbo.distribday_trace dt join dbo.bankaccountterminal t on dt.terminalid = t.terminalid
+ where t.mainsubdealer = 7
+   and dt.ddate = convert(varchar(10), @date, 121)
+   and t.src_system in (1,2)
+   and dt.today_summ > @summ
+   and exists(select * 
+                from dbo.paysystempayments
+               where bank_ddate = dt.ddate
+                 and terminalid = dt.terminalid
+                 and paysystem = 9000000)
+ order by case when today_summ > @summ*3 then 0 else 1 end, today_summ % 100 desc
+)
