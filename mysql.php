98f54669f42e434a0f2f6e1875eaa0239981cf8a
Alexander Levin <alevin.ru@gmail.com>
2012-10-11 18:58:53 +0400

    Форматирование

--- mysql.php
+++ mysql.php
@@ -20,15 +20,16 @@
 	$args=$_POST;
 
 	if (isset($args['request'])) try {
-
+		
 		$link = mysql_connect($args['server'],$args['login'], $args['pwd']);
 		if (!$link) throw new Exception("Can't connect to server. Server returned:".mysql_error());
 		
 		if ($args['db'] && $args['db']!='') {
 			$db_selected = mysql_select_db($args['db'],$link);
 		}
+		
 		if (!$db_selected) throw new Exception("Can't select database. Server returned:".mysql_error());
-
+		
 		$q=new SimpleXMLElement($args['request']);
 		$sql=(string)$q->sql[0];
 		
@@ -37,27 +38,27 @@
 		$query = mysql_query($sql);
 		
 		if (!$query) throw new Exception(mysql_error());
-
+		
 		$result = new DOMDocument('1.0','UTF-8');
-
-	        if (!is_bool($query)) {
-            		$row = mysql_fetch_array($query);
-            		$output->documentElement->setAttribute('result-size',strlen($row[0]));
-           		$result->loadXML('<result-set>'.$row[0].'</result-set>');
-	            	mysql_free_result($query);
-        	}
-        	else {
-        	    	$sql='select row_count();';
-            		$query = mysql_query($sql);
-	            	$row = mysql_fetch_array($query);
-        	    	$result->loadXML('<rows-affected>'.$row[0].'</rows-affected>');
-            		mysql_free_result($query);
-        	}
+		
+		if (!is_bool($query)) {
+				$row = mysql_fetch_array($query);
+				$output->documentElement->setAttribute('result-size',strlen($row[0]));
+				$result->loadXML('<result-set>'.$row[0].'</result-set>');
+				mysql_free_result($query);
+		} else {
+				$sql='select row_count();';
+				$query = mysql_query($sql);
+				$row = mysql_fetch_array($query);
+				$result->loadXML('<rows-affected>'.$row[0].'</rows-affected>');
+				mysql_free_result($query);
+		}
+		
 		$output->documentElement->appendChild($output->importNode($result->documentElement, true));
 		
-        } catch (Exception $e) {
+    } catch (Exception $e) {
 		$output->documentElement->appendChild($output->createElement('exception',$e->getMessage()));
-    	}
+    }
 	
 	$output->documentElement->setAttribute('xmlns','http://unact.net/xml/xi');
  
