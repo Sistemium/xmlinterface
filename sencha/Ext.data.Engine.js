4702f352e9560e4c69a8712de6035c7ac7e49dfa
Alexander Levin <alevin.ru@gmail.com>
2013-10-10 14:47:54 +0400

    sencha db limit

--- sencha/Ext.data.Engine.js
+++ sencha/Ext.data.Engine.js
@@ -105,6 +105,14 @@ Ext.data.Engine = Ext.extend(Ext.util.Observable, {
             }
         ;
         
+        var iOS = parseFloat(
+			('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
+			.replace('undefined', '3_2').replace('_', '.').replace('_', '')
+			) || false
+		;
+		
+		if (iOS >= 7) targetSize = 1.0 / 1024.0;
+        
         me.metadata = metadata;
         me.tables = me.metadata.tables;
         
@@ -129,6 +137,7 @@ Ext.data.Engine = Ext.extend(Ext.util.Observable, {
             var shortName = name;
             var displayName = name;
             var maxSize = targetSize*1024*1024; // in bytes
+            
             var db = openDatabase(shortName, "", displayName, maxSize);
             
             if (db.version<targetVersion || forceRebuild)
