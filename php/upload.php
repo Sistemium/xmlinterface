60349b5862d768035389cbfe1d16278c522f574b
Alexander Levin <alevin.ru@gmail.com>
2018-08-23 22:38:08 +0300

    safe upload

--- php/upload.php
+++ php/upload.php
@@ -18,16 +18,16 @@
     }
 
     set_error_handler( 'http_exceptions_error_handler' );
-    
-    
+
+
 
     set_time_limit(180);
-    
+
     $rawPost = file_get_contents('php://input');
 
     $headers = apache_request_headers ();
     $authToken = $headers ['Authorization'];
-    
+
     if (!$authToken) {
         header ('System-error: 401',1,401);
         $path = localPath('../data/upload/' . uniqid('err.401-') . '.headers.txt');
@@ -35,7 +35,7 @@
         file_put_contents ($path, $headersString);
         die ('Unauthorized'."\n");
     }
-    
+
     if ($_SERVER['CONTENT_TYPE'] != 'text/xml') {
         header ('System-error: 400',1,400);
         $path = localPath('../data/upload/' . uniqid('err.400-') . '.headers.txt');
@@ -45,22 +45,22 @@
         file_put_contents ($path, $rawPost);
         die ("'Content-type' must be text/xml\n");
     }
-    
+
     session_name('UOAuthClient');
     session_id($authToken);
     session_start();
-    
+
     $uac = new UOAuthClient ('http://uoauth.sistemium.net/a/uoauth', $authToken);
-    
+
     session_write_close();
-    
+
     $org = $uac->hasRole('org');
-    
+
     if (!$org) {
         header ('System-error: 403',1,403);
         die ('<h1>Not authorized</h1>'."\n");
     }
-    
+
     if ($rawPost == '') {
         header ('System-error: 400',1,400);
         die ("Post data not found\n");
@@ -68,7 +68,7 @@
 
     $siteRole = $uac->hasRole('site');
 
-    $site = isset($_REQUEST['site']) ? $_REQUEST['site'] : false;
+    $site = isset($_REQUEST['site']) ? $_REQUEST['site'] : $siteRole;
 
     if (!$site) {
         $site = getLastPathSegment ($_SERVER['REQUEST_URI']);
@@ -81,50 +81,58 @@
         header ('System-error: 401',1,401);
         die ('<h1>Not authorized on this site</h1>'."\n");
     }
-    
-    $xml = new DOMDocument ('1.0');
-    $xml->loadXML($rawPost);
-    $instructions = new DOMDocument ('1.0');
-    $instructions -> load (localPath('../../config/xsl/upload.instructions.xsl'));
-
-    $params = array("org" => $org);
 
     if ($site) {
         $params['site'] = $site;
     }
 
-    $matrix = new XSLTWhatMatrix (
-        $xml,
-        $instructions,
-        $params
-    );
+    $logFolder = '../data/upload/' . $org . '.';
 
-    $matrix->auth = $authToken;
-    $matrix->resultPath = '../data/upload/' . $org . '.';
-    
-    file_put_contents($matrix->resultPath . uniqid('log-') . '.rawpost.xml', $rawPost);
-    
-    if (!$matrix -> instructions) {
-        header ('System-error: 500',1,500);
-        die("Response matrix\n");
+    try {
+
+        $params = array("org" => $org);
+        file_put_contents($logFolder . uniqid('log-') . '.rawpost.xml', $rawPost);
+
+        $xml = new DOMDocument ('1.0');
+        $xml->loadXML($rawPost);
+        $instructions = new DOMDocument ('1.0');
+        $instructions -> load (localPath('../../config/xsl/upload.instructions.xsl'));
+
+        $matrix = new XSLTWhatMatrix (
+            $xml,
+            $instructions,
+            $params
+        );
+
+        $matrix->auth = $authToken;
+        $matrix->resultPath = $logFolder;
+
+        if (!$matrix -> instructions) {
+            header ('System-error: 500',1,500);
+            die("Response matrix\n");
+        }
+
+        $instructions = simplexml_import_dom ($matrix -> instructions);
+        $jobDone = $matrix ->apply ($instructions, $xml);
+
+        if (!$jobDone) {
+            header ('System-error: 500',1,500);
+            die("Server busy\n");
+        }
+
+        header('Content-type: text/xml', true);
+
+        $dom = dom_import_simplexml($instructions) -> ownerDocument;
+        $dom->formatOutput = true;
+
+        $result = $dom->saveXML();
+        file_put_contents($matrix->resultPath . uniqid('log-') . '.result.xml', $result);
+
+        print $result;
+
+    } catch (Exception $e) {
+        header ('System-error: 500', 1, 500);
+        die ($e->getMessage());
     }
-    
-    $instructions = simplexml_import_dom ($matrix -> instructions);
-    $jobDone = $matrix ->apply ($instructions, $xml);
-    
-    if (!$jobDone) {
-        header ('System-error: 500',1,500);
-        die("Server busy\n");
-    }
-    
-    header('Content-type: text/xml', true);
-    
-    $dom = dom_import_simplexml($instructions) -> ownerDocument;
-    $dom->formatOutput = true;
-    
-    $result = $dom->saveXML();
-    file_put_contents($matrix->resultPath . uniqid('log-') . '.result.xml', $result);
-    
-    print $result;
-    
+
 ?>
