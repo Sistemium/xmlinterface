919b4d3bdec21fba64d68a0f97c6cfb3c54bd50f
Alexander Levin <alevin.ru@gmail.com>
2018-08-23 22:40:41 +0300

    spacing

--- php/HTTPClient.php
+++ php/HTTPClient.php
@@ -1,18 +1,18 @@
 <?php
 
     class HTTPClient {
-        
+
         static function post ($url, $data  = array(), $headers = array()) {
-            
+
             $curlRequest = curl_init( $url );
-            
+
             curl_setopt($curlRequest, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curlRequest, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, true);
-            
             curl_setopt($ch, CURLOPT_TIMEOUT, 160);
+
             $dataToPost = '';
-            
+
             if (is_array($data)) {
                 foreach ($data as $key=>$value)
                     $dataToPost .= ($dataToPost?'&':'').$key .'='. $value
@@ -21,7 +21,7 @@
                 $headers ['Content-Type'] = 'text/xml';
                 $dataToPost = $data;
             }
-            
+
             if (count($headers)){
                 $headersToSend = array();
                 foreach ($headers as $key=>$value)
@@ -29,24 +29,24 @@
                 ;
                 curl_setopt($curlRequest, CURLOPT_HTTPHEADER, $headersToSend);
             }
-            
+
             if ($dataToPost!='') {
                 curl_setopt($curlRequest, CURLOPT_POSTFIELDS, $dataToPost);
             }
-            
+
             $result = curl_exec($curlRequest);
-            
+
             $httpStatus = curl_getinfo($curlRequest, CURLINFO_HTTP_CODE);
-            
+
             curl_close($curlRequest);
-            
+
             if ($httpStatus != 200) {
                 throw new ErrorException ($result, $httpStatus);
             }
-            
+
             return $result;
-            
+
         }
-        
+
     }
 ?>
